a7afc878d058ee2d980265d43f0da5bc21829f63
zataara <bryantmac@gmail.com>
2021-09-18 12:39:34 -0700

    setting up template db

---
+++ db.js
@@ -0,0 +1,20 @@
+const { Client } = requre("pg");
+
+let DB_URI;
+
+if (process.env.NODE_ENF === "test") {
+    DB_URI = "postgresql:///usersdb_test";
+} else {
+    DB_URI = "postgresql:///usersdb";
+}
+
+let db = new Client({
+    connectionString: DB_URI
+});
+
+db.connect();
+
+
+
+
+module.exports = db;
