e0a6c10ace438f49cbe98bb96b2b7ac0fb0eece0
zataara <bryantmac@gmail.com>
2021-09-18 23:59:44 -0700

    added 404 handler

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.json());
 const uRoutes = require("./routes/users");
 app.use("/users", uRoutes);
 
+/* 404 HANDLER */ 
 app.use(function(req, res, next) {
     const err = new ExpressError("Not Found", 404);
     return next(err)
