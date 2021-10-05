69d037ad0766181e11e7fc982f45c2bc58c00282
zataara <bryantmac@gmail.com>
2021-10-05 14:27:02 -0700

    moving logic to config file

--- db.js
+++ db.js
@@ -1,17 +1,10 @@
-const { Client } = requre("pg");
+const { Client } = require("pg");
+const { DB_URI } = require("./config")
 
-let DB_URI;
-
-if (process.env.NODE_ENF === "test") {
-    DB_URI = "postgresql:///usersdb_test";
-} else {
-    DB_URI = "postgresql:///usersdb";
-}
-
-let db = new Client({
-    connectionString: DB_URI
+const client = new Client({
+    connectionstring: DB_URI
 });
 
-db.connect();
+client.connect();
 
-module.exports = db;
+module.exports = client;
